64ca83c2591b940804f1aaac8959963e1e94001f
ishakzail <ishak.zail.iz@gmail.com>
2021-11-11 21:00:13 +0100

    update libft.h

--- libft.h
+++ libft.h
@@ -6,8 +6,47 @@
 /*   By: izail <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 18:35:21 by izail             #+#    #+#             */
-/*   Updated: 2021/11/09 18:35:23 by izail            ###   ########.fr       */
+/*   Updated: 2021/11/11 11:45:32 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBFT_H
+# define LIBFT_H
+# include<stddef.h>
+# include<stdlib.h>
+# include<string.h>
 
+int		ft_atoi(char *str);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+int		ft_strncmp(const char *s1, size_t n);
+int		ft_strlen(char *str);
+size_t	ft_strlcpy(char *restrict dst,
+			const char *restrict src, size_t dstsize);
+size_t	ft_strlcat(char *restrict dst,
+			const char *restrict src, size_t dstsize);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		*ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *str, int c, size_t n);
+
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+#endif
