6ad1880cb8b824ac310cc46e216c5e350cf24851
ishakzail <ishak.zail.iz@gmail.com>
2021-11-15 16:55:27 +0100

    Fixed

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -21,7 +21,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	s11 = (unsigned char *)s1;
 	s22 = (unsigned char *)s2;
 	i = 0;
-	while (i < n && s11[i] == s22[i])
+	while (i < n)
     {
 		if (s11[i] != s22[i])
 			return (s11[i] - s22[i]);
