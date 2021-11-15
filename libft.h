98b4a60016a4e9a941e053a93662249dffe717c0
ishakzail <ishak.zail.iz@gmail.com>
2021-11-15 16:55:44 +0100

    updated

--- libft.h
+++ libft.h
@@ -45,7 +45,7 @@ void    ft_striteri(char *s, void (*f)(unsigned int,char*));
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
-int		*ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *str, int c, size_t n);
