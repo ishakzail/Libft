20896f657475116083a7f6d31c29d3c7ac0a80db
ishakzail <ishak.zail.iz@gmail.com>
2021-11-16 23:36:32 +0100

    updated for bonus part

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: izail <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 18:35:21 by izail             #+#    #+#             */
-/*   Updated: 2021/11/15 18:54:35 by izail            ###   ########.fr       */
+/*   Updated: 2021/11/16 19:45:18 by izail            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # include<stdlib.h>
 # include<string.h>
 # include<unistd.h>
+
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 int		ft_atoi(const char *str);
 int		ft_isalnum(int c);
@@ -55,4 +61,14 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list 	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list 	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *));
 #endif
